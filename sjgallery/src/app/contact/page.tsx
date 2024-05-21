fd3cf41ab0582c5a11a9a337c09e8d35b0939f8c
nwcassiani <nwcassiani@gmail.com>
2024-05-21 16:02:51 -0500

    contact page info

--- sjgallery/src/app/contact/page.tsx
+++ sjgallery/src/app/contact/page.tsx
@@ -1,3 +1,21 @@
 export default function Page() {
-  return <p>Contact Page</p>;
+  return (
+    <div className="contact">
+      <div>
+        <h3>Address</h3>
+        <p>2534 Esplanade Ave</p>
+        <p>New Orleans, LA 70119</p>
+      </div>
+      <br />
+      <div>
+        <h3>Email</h3>
+        <p>gallery@sarahjanko.com</p>
+      </div>
+      <br />
+      <div>
+        <h3>Hours</h3>
+        <p>By Appointment Only</p>
+      </div>
+    </div>
+  )
 }
