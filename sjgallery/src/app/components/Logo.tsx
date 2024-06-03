d2c138b12afff65c0c6a5d0f04c3ab25a864c5e6
nwcassiani <nwcassiani@gmail.com>
2024-06-03 16:10:40 -0500

    testing logo blink animation

--- sjgallery/src/app/components/Logo.tsx
+++ sjgallery/src/app/components/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = () => {
       <div className="logo-letter" style={{ paddingTop: '50px'}}>
         <h1>J</h1>
       </div>
-      <div className="logo-letter">
+      <div className="logo-letter" >
         <h1>G</h1>
       </div>
       <div className="logo-lash" style={{ 
@@ -26,6 +26,36 @@ const Logo = () => {
       <div className="logo-lash" style={{ 
         right: '78px',
         transform: 'rotate(40deg)'
+        }}>
+        <h2>I</h2>
+      </div>
+      <div 
+          className="logo-letter blink" 
+          style={{
+            transform: 'rotate(180deg) scaleY(-1)',
+            // visibility: 'hidden'
+          }}
+        >
+        <h1>G</h1>
+      </div>
+      <div className="logo-lash blink" style={{ 
+        right: '125px',
+        top: '68px',
+        transform: 'rotate(-140deg) scaleY(-1)'
+        }}>
+        <h2>I</h2>
+      </div>
+      <div className="logo-lash blink" style={{ 
+        right: '52px',
+        paddingTop: '55px',
+        transform: 'scaleY(-1)'
+        }}>
+        <h2>I</h2>
+      </div>
+      <div className="logo-lash blink" style={{ 
+        right: '40px',
+        paddingTop: '63px',
+        transform: 'rotate(140deg)'
         }}>
         <h2>I</h2>
       </div>
