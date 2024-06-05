87af2c1c546ea06e1c2e2d159198320989ce1bc7
nwcassiani <nwcassiani@gmail.com>
2024-06-05 14:06:59 -0500

    install swiper and init images lib file

---
+++ sjgallery/lib/images.ts
@@ -0,0 +1,9 @@
+export const images = [
+  { src: "https://res.cloudinary.com/dsxmv5yjt/image/upload/v1716498248/L1150033-2_hxfvfe.jpg",
+    alt: "first"
+  },
+  { src: "https://res.cloudinary.com/dsxmv5yjt/image/upload/v1717614035/ptg3_qglrst.jpg",
+    alt: "second"},
+  { src: "https://res.cloudinary.com/dsxmv5yjt/image/upload/v1717614323/New_Moon-11_zf6kdm.jpg",
+    alt: "third"}
+];
